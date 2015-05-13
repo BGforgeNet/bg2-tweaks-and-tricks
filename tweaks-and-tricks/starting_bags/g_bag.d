33eece554b35e5b938b39c4d545099eac3002363
Ilya Ivanov <forn@ngs.ru>
2015-05-13 16:48:09 +0700

    rename bags component. Remove some tabs from files.

--- tweaks-and-tricks/starting_bags/g_bag.d
+++ tweaks-and-tricks/starting_bags/g_bag.d
@@ -1,14 +1,14 @@
 BEGIN ~Bags~
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
-	SAY ~<CHARNAME>, wait. I've been told that you are leaving Candlekeep.~IF ~~ THEN REPLY ~Hello, Thorius. Yes, Gorion told me to prepare for a travel.~ GOTO 1
+  SAY ~<CHARNAME>, wait. I've been told that you are leaving Candlekeep.~IF ~~ THEN REPLY ~Hello, Thorius. Yes, Gorion told me to prepare for a travel.~ GOTO 1
 IF ~~ THEN REPLY ~Get lost, old man. I've no time.~ GOTO 2
 END
 IF ~~ THEN BEGIN 1
-	SAY ~You have always been my favorite pupil, I want to help you whatever I can. Take these things, it's not weapon or armor, but experienced adventurers know its real value. And I have no need in it here.~
+  SAY ~You have always been my favorite pupil, I want to help you however I can. Take these things, it's not weapon or armor, but experienced adventurers know their real value. And I have no need in it here.~
 IF ~~ THEN REPLY ~I'm very grateful for your lessons and your help, Thorius.~ DO ~SetNumTimesTalkedTo(0) GiveItemCreate("g_gem",Player1,1,1,1) GiveItemCreate("g_pot",Player1,1,1,1) GiveItemCreate("g_scr",Player1,1,1,1) EscapeArea()~
 EXIT
 END
 IF ~~ THEN BEGIN 2
-	SAY ~But...Well, I see you have no need in my help.~ 
+  SAY ~But... Well, I see you have no need of my help.~ 
 IF ~~ THEN DO ~SetNumTimesTalkedTo(0) EscapeArea()~  EXIT
 END
