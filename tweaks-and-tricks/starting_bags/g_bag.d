c6ef017631423a4ba794dbfa99b9028c67319056
Ilya Ivanov <forn@ngs.ru>
2015-05-13 19:58:28 +0700

    expand starting bags to bg2

--- tweaks-and-tricks/starting_bags/g_bag.d
+++ tweaks-and-tricks/starting_bags/g_bag.d
@@ -1,11 +1,11 @@
-BEGIN ~Bags~
+BEGIN ~g_bags~
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
   SAY ~<CHARNAME>, wait. I've been told that you are leaving Candlekeep.~IF ~~ THEN REPLY ~Hello, Thorius. Yes, Gorion told me to prepare for a travel.~ GOTO 1
 IF ~~ THEN REPLY ~Get lost, old man. I've no time.~ GOTO 2
 END
 IF ~~ THEN BEGIN 1
-  SAY ~You have always been my favorite pupil, I want to help you however I can. Take these things, it's not weapon or armor, but experienced adventurers know their real value. And I have no need in it here.~
-IF ~~ THEN REPLY ~I'm very grateful for your lessons and your help, Thorius.~ DO ~SetNumTimesTalkedTo(0) GiveItemCreate("g_gem",Player1,1,1,1) GiveItemCreate("g_pot",Player1,1,1,1) GiveItemCreate("g_scr",Player1,1,1,1) EscapeArea()~
+  SAY ~You have always been my favorite pupil, I want to help you however I can. Take these things. It's no weapon or armor, but experienced adventurers know their real value.~
+IF ~~ THEN REPLY ~I'm very grateful for your lessons and your help, Thorius.~ DO ~SetNumTimesTalkedTo(0) %action_bg1_bags% EscapeArea()~
 EXIT
 END
 IF ~~ THEN BEGIN 2
