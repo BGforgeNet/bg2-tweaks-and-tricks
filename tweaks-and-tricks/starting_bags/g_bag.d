29caf47ef63a12c1ac7377a620667e415136e1b7
Ilya Ivanov <ilya@devopsx.com>
2016-03-09 18:24:26 +0700

    shorten THEN BEGIN to BEGIN everywhere

--- tweaks-and-tricks/starting_bags/g_bag.d
+++ tweaks-and-tricks/starting_bags/g_bag.d
@@ -1,14 +1,14 @@
 BEGIN ~g_bags~
-IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
+IF ~NumTimesTalkedTo(0)~ BEGIN 0
   SAY ~<CHARNAME>, wait. I've been told that you are leaving Candlekeep.~IF ~~ THEN REPLY ~Hello, Thorius. Yes, Gorion told me to prepare for a travel.~ GOTO 1
 IF ~~ THEN REPLY ~Get lost, old man. I've no time.~ GOTO 2
 END
-IF ~~ THEN BEGIN 1
+IF ~~ BEGIN 1
   SAY ~You have always been my favorite pupil, I want to help you however I can. Take these things. It's no weapon or armor, but experienced adventurers know their real value.~
 IF ~~ THEN REPLY ~I'm very grateful for your lessons and your help, Thorius.~ DO ~SetNumTimesTalkedTo(0) %action_bg1_bags% EscapeArea()~
 EXIT
 END
-IF ~~ THEN BEGIN 2
+IF ~~ BEGIN 2
   SAY ~But... Well, I see you have no need of my help.~ 
 IF ~~ THEN DO ~SetNumTimesTalkedTo(0) EscapeArea()~  EXIT
 END
