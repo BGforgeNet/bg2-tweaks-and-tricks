a8b50110772115a7570ce72c4e3f3b0a5ddc230a
Magus <magus@bgforge.net>
2020-02-10 14:07:31 +0700

    use ielib in fair fights

--- tnt/fair_fights/malchor.d
+++
@@ -1,6 +0,0 @@
-REPLACE_ACTION_TEXT c6harp
-  ~SetGlobal("TalkedToMalchor","GLOBAL",3).*~
-  ~SetGlobal("TalkedToMalchor","GLOBAL",3)
-   ENEMY()~
-
-REPLACE_ACTION_TEXT c6harp ~ReallyForceSpell.*~ ~~  //imprisonment action
