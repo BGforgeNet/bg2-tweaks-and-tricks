1af9be06e7abb33d6f5bd49a567b5cf0e1bc0a8e
Magus <magus@bgforge.net>
2023-01-31 21:33:46 +0700

    rearrange familiars tra refs

--- tnt/components/smarter_familiars/familiars.d
+++ tnt/components/smarter_familiars/familiars.d
@@ -1,124 +1,126 @@
+/** @tra smarter_familiars.tra */
+
 EXTEND_TOP %dialog% %state%
-  +~InPartySlot(LastTalkedToBy,0)~+ @167 + g_familiar_follow
-  +~InPartySlot(LastTalkedToBy,0)~+ @169 + g_familiar_combat
-  +~InPartySlot(LastTalkedToBy,0)~+ @226 + g_familiar_potions
-  +~InPartySlot(LastTalkedToBy,0)~+ @168 + g_familiar_loot
+  +~InPartySlot(LastTalkedToBy,0)~+ @101 + g_familiar_follow
+  +~InPartySlot(LastTalkedToBy,0)~+ @102 + g_familiar_combat
+  +~InPartySlot(LastTalkedToBy,0)~+ @103 + g_familiar_potions
+  +~InPartySlot(LastTalkedToBy,0)~+ @104 + g_familiar_loot
 END
 
 
 APPEND %dialog%
   IF ~~ g_familiar_follow
-    SAY @170
+    SAY @100
 
     + ~Global("g_FamFollowMaster","GLOBAL",0)~
-    + @171 DO ~SetGlobal("g_FamFollowMaster","GLOBAL",1)~
+    + @201 DO ~SetGlobal("g_FamFollowMaster","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamFollowMaster","GLOBAL",1)~
-    + @172 DO ~SetGlobal("g_FamFollowMaster","GLOBAL",0)~
+    + @202 DO ~SetGlobal("g_FamFollowMaster","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~Global("g_FamSpeed","GLOBAL",0)~
-    + @212 DO ~SetGlobal("g_FamSpeed","GLOBAL",1)
+    + @203 DO ~SetGlobal("g_FamSpeed","GLOBAL",1)
                ApplySpellRES("G_FSPED1","%death_var%")~
     + g_familiar_confirm
 
     + ~Global("g_FamSpeed","GLOBAL",1)~
-    + @213 DO ~SetGlobal("g_FamSpeed","GLOBAL",0)
+    + @204 DO ~SetGlobal("g_FamSpeed","GLOBAL",0)
                ApplySpellRES("G_FSPED0","%death_var%")~
     + g_familiar_confirm
 
-    ++ @173 + %state%
+    ++ @205 + %state%
   END
 END
 
 
 APPEND %dialog%
   IF ~~ g_familiar_combat
-    SAY @170
+    SAY @100
 
     + ~Global("g_FamJumpToPack","GLOBAL",0)~ //hide on sight
-    + @174 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",1)
+    + @301 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",1)
                SetGlobal("g_FamJumpToPackInjured","GLOBAL",0)
                SetGlobal("g_FamJumpToPackBadlyInjured","GLOBAL",0)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamJumpToPack","GLOBAL",1)~ //never hide
-    + @175 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",0)
+    + @302 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",0)
                SetGlobal("g_FamJumpToPackInjured","GLOBAL",0)
                SetGlobal("g_FamJumpToPackBadlyInjured","GLOBAL",0)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamJumpToPackInjured","GLOBAL",0)~ //hide when injured
-    + @176 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",0)
+    + @303 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",0)
                SetGlobal("g_FamJumpToPackInjured","GLOBAL",1)
                SetGlobal("g_FamJumpToPackBadlyInjured","GLOBAL",0)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamJumpToPackBadlyInjured","GLOBAL",0)~ //hide when badly injured
-    + @177 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",0)
+    + @304 DO ~SetGlobal("g_FamJumpToPack","GLOBAL",0)
                SetGlobal("g_FamJumpToPackInjured","GLOBAL",0)
                SetGlobal("g_FamJumpToPackBadlyInjured","GLOBAL",1)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamiliarJumpOut","GLOBAL",0)~ //jump out self
-    + @178 DO ~SetGlobal("g_FamiliarJumpOut","GLOBAL",1)~
+    + @306 DO ~SetGlobal("g_FamiliarJumpOut","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamiliarJumpOut","GLOBAL",1)~ //don't ever jump out
-    + @179 DO ~SetGlobal("g_FamiliarJumpOut","GLOBAL",0)~
+    + @307 DO ~SetGlobal("g_FamiliarJumpOut","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~OR(2)
          Global("g_FamHurtNotif","GLOBAL",1)
          Global("g_FamBadlyHurtNotif","GLOBAL",1)~ //no Notif when hurt
-    + @207 DO ~SetGlobal("g_FamHurtNotif","GLOBAL",0)
+    + @309 DO ~SetGlobal("g_FamHurtNotif","GLOBAL",0)
                SetGlobal("g_FamBadlyHurtNotif","GLOBAL",0)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamHurtNotif","GLOBAL",0)~ //Notif when hurt
-    + @208 DO ~SetGlobal("g_FamHurtNotif","GLOBAL",1)
+    + @310 DO ~SetGlobal("g_FamHurtNotif","GLOBAL",1)
                SetGlobal("g_FamBadlyHurtNotif","GLOBAL",0)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamBadlyHurtNotif","GLOBAL",0)~ //Notif when badly hurt
-    + @209 DO ~SetGlobal("g_FamHurtNotif","GLOBAL",0)
+    + @311 DO ~SetGlobal("g_FamHurtNotif","GLOBAL",0)
                SetGlobal("g_FamBadlyHurtNotif","GLOBAL",1)
               ~
     + g_familiar_confirm
 
     + ~Global("g_FamHitAndRun","GLOBAL",0)~ //hit and run on
-    + @221 DO ~SetGlobal("g_FamHitAndRun","GLOBAL",1)~
+    + @312 DO ~SetGlobal("g_FamHitAndRun","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamHitAndRun","GLOBAL",1)~ //hit and run off
-    + @222 DO ~SetGlobal("g_FamHitAndRun","GLOBAL",0)~
+    + @313 DO ~SetGlobal("g_FamHitAndRun","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~!Global("g_FamTactic","GLOBAL",0)~ //combat tactic: do nothing
-    + @223 DO ~SetGlobal("g_FamTactic","GLOBAL",0)~
+    + @314 DO ~SetGlobal("g_FamTactic","GLOBAL",0)~
     + g_familiar_confirm
     + ~!Global("g_FamTactic","GLOBAL",1)~ //combat tactic: attack nearest
-    + @224 DO ~SetGlobal("g_FamTactic","GLOBAL",1)~
+    + @315 DO ~SetGlobal("g_FamTactic","GLOBAL",1)~
     + g_familiar_confirm
     + ~!Global("g_FamTactic","GLOBAL",2)~ //combat tactic: attack nearest spellcaster
-    + @225 DO ~SetGlobal("g_FamTactic","GLOBAL",2)~
+    + @316 DO ~SetGlobal("g_FamTactic","GLOBAL",2)~
     + g_familiar_confirm
 
-    ++ @180 + %state%
+    ++ @105 + %state%
   END
 END
 
 
 APPEND %dialog%
   IF ~~ g_familiar_potions
-    SAY @226
+    SAY @104
 
 /* For some reason, the whole stack of goodberries gets removed
     + ~PartyHasItem("gberry")~ //goodberry
@@ -208,7 +210,7 @@ APPEND %dialog%
 */
     //generated in main.tpa, a state for each potion
     %action_drink%
-    ++ @180 + %state%
+    ++ @105 + %state%
   END
 END
 
@@ -216,9 +218,9 @@ END
 
 APPEND %dialog%
   IF ~~ g_familiar_loot
-    SAY @170
+    SAY @100
 
-    ++ @181
+    ++ @400
       DO ~SetGlobal("g_FamPickupGold","GLOBAL",0)
           SetGlobal("g_FamPickupJewelsCommon","GLOBAL",0)
           SetGlobal("g_FamPickupJewelsRare","GLOBAL",0)
@@ -231,7 +233,7 @@ APPEND %dialog%
          ~
     + g_familiar_confirm
 
-    ++ @182
+    ++ @401
       DO ~SetGlobal("g_FamPickupGold","GLOBAL",1)
           SetGlobal("g_FamPickupJewelsCommon","GLOBAL",1)
           SetGlobal("g_FamPickupJewelsRare","GLOBAL",1)
@@ -244,29 +246,29 @@ APPEND %dialog%
          ~
     + g_familiar_confirm
 
-    +~Global("g_FamPickupGold","GLOBAL",0)~+ @183 DO ~SetGlobal("g_FamPickupGold","GLOBAL",1)~ + g_familiar_confirm
-    +~Global("g_FamPickupGold","GLOBAL",1)~+ @184 DO ~SetGlobal("g_FamPickupGold","GLOBAL",0)~ + g_familiar_confirm
+    +~Global("g_FamPickupGold","GLOBAL",0)~+ @402 DO ~SetGlobal("g_FamPickupGold","GLOBAL",1)~ + g_familiar_confirm
+    +~Global("g_FamPickupGold","GLOBAL",1)~+ @403 DO ~SetGlobal("g_FamPickupGold","GLOBAL",0)~ + g_familiar_confirm
 
     + ~OR(3)
         Global("g_FamPickupJewelsCommon","GLOBAL",0)
         Global("g_FamPickupJewelsRare","GLOBAL",0)
         Global("g_FamPickupJewelsMagic","GLOBAL",0)
       ~
-    + @185
+    + @404
       DO ~SetGlobal("g_FamPickupJewelsCommon","GLOBAL",1)
           SetGlobal("g_FamPickupJewelsRare","GLOBAL",1)
           SetGlobal("g_FamPickupJewelsMagic","GLOBAL",1)
          ~
     + g_familiar_confirm
 
-    ++ @186
+    ++ @405
         DO ~SetGlobal("g_FamPickupJewelsCommon","GLOBAL",0)
         SetGlobal("g_FamPickupJewelsRare","GLOBAL",1)
         SetGlobal("g_FamPickupJewelsMagic","GLOBAL",1)
        ~
     + g_familiar_confirm
 
-    ++ @187
+    ++ @406
         DO ~SetGlobal("g_FamPickupJewelsCommon","GLOBAL",0)
         SetGlobal("g_FamPickupJewelsRare","GLOBAL",0)
         SetGlobal("g_FamPickupJewelsMagic","GLOBAL",1)
@@ -278,7 +280,7 @@ APPEND %dialog%
         Global("g_FamPickupJewelsRare","GLOBAL",1)
         Global("g_FamPickupJewelsMagic","GLOBAL",1)
       ~
-    + @188
+    + @407
         DO ~SetGlobal("g_FamPickupJewelsCommon","GLOBAL",0)
         SetGlobal("g_FamPickupJewelsRare","GLOBAL",0)
         SetGlobal("g_FamPickupJewelsMagic","GLOBAL",0)
@@ -286,52 +288,52 @@ APPEND %dialog%
     + g_familiar_confirm
 
     + ~Global("g_FamPickupAmmo","GLOBAL",0)~
-    + @189 DO ~SetGlobal("g_FamPickupAmmo","GLOBAL",1)~
+    + @408 DO ~SetGlobal("g_FamPickupAmmo","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupAmmo","GLOBAL",1)~
-    + @190 DO ~SetGlobal("g_FamPickupAmmo","GLOBAL",0)~
+    + @409 DO ~SetGlobal("g_FamPickupAmmo","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupPotions","GLOBAL",0)~
-    + @191 DO ~SetGlobal("g_FamPickupPotions","GLOBAL",1)~
+    + @410 DO ~SetGlobal("g_FamPickupPotions","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupPotions","GLOBAL",1)~
-    + @192 DO ~SetGlobal("g_FamPickupPotions","GLOBAL",0)~
+    + @411 DO ~SetGlobal("g_FamPickupPotions","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupScrolls","GLOBAL",0)~
-    + @193 DO ~SetGlobal("g_FamPickupScrolls","GLOBAL",1)~
+    + @412 DO ~SetGlobal("g_FamPickupScrolls","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupScrolls","GLOBAL",1)~
-    + @194 DO ~SetGlobal("g_FamPickupScrolls","GLOBAL",0)~
+    + @413 DO ~SetGlobal("g_FamPickupScrolls","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupScalps","GLOBAL",0)~
-    + @195 DO ~SetGlobal("g_FamPickupScalps","GLOBAL",1)~
+    + @414 DO ~SetGlobal("g_FamPickupScalps","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupScalps","GLOBAL",1)~
-    + @196 DO ~SetGlobal("g_FamPickupScalps","GLOBAL",0)~
+    + @415 DO ~SetGlobal("g_FamPickupScalps","GLOBAL",0)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupTrophy","GLOBAL",0)~
-    + @197 DO ~SetGlobal("g_FamPickupTrophy","GLOBAL",1)~
+    + @416 DO ~SetGlobal("g_FamPickupTrophy","GLOBAL",1)~
     + g_familiar_confirm
 
     + ~Global("g_FamPickupTrophy","GLOBAL",1)~
-    + @198 DO ~SetGlobal("g_FamPickupTrophy","GLOBAL",0)~
+    + @417 DO ~SetGlobal("g_FamPickupTrophy","GLOBAL",0)~
     + g_familiar_confirm
 
-    ++ @199 + %state%
+    ++ @418 + %state%
 
   END
 
 
   IF ~~ g_familiar_confirm
-    SAY @200
+    SAY @419
   IF ~~ + %state%
   END
 
