3ee5d0db7183eaa971ffe1fafc67cd9f8ea913e2
Magus <magus@bgforge.net>
2023-02-28 17:16:17 +0700

    traify starting bags

--- tnt/components/starting_bags/g_bags.d
+++ tnt/components/starting_bags/g_bags.d
@@ -1,14 +1,18 @@
+/** @tra starting_bags.tra */
+
 BEGIN ~%cre_id%~
 IF ~NumTimesTalkedTo(0)~ BEGIN 0
-  SAY ~<CHARNAME>, wait. I've been told that you are leaving Candlekeep.~IF ~~ THEN REPLY ~Hello, Thorius. Yes, Gorion told me to prepare for a travel.~ GOTO 1
-IF ~~ THEN REPLY ~Get lost, old man. I've no time.~ GOTO 2
+  SAY @20
+    IF ~~ THEN REPLY @21 GOTO 1
+    IF ~~ THEN REPLY @22 GOTO 2
 END
+
 IF ~~ BEGIN 1
-  SAY ~You have always been my favorite pupil, I want to help you however I can. Take these things. It's no weapon or armor, but experienced adventurers know their real value.~
-IF ~~ THEN REPLY ~I'm very grateful for your lessons and your help, Thorius.~ DO ~SetNumTimesTalkedTo(0) %action_bg1_bags% EscapeArea()~
-EXIT
+  SAY @23
+  IF ~~ THEN REPLY @24 DO ~SetNumTimesTalkedTo(0) %action_bg1_bags% EscapeArea()~ EXIT
 END
+
 IF ~~ BEGIN 2
-  SAY ~But... Well, I see you have no need of my help.~ 
-IF ~~ THEN DO ~SetNumTimesTalkedTo(0) EscapeArea()~  EXIT
+  SAY @25
+  IF ~~ THEN DO ~SetNumTimesTalkedTo(0) EscapeArea()~  EXIT
 END
