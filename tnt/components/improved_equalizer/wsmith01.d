a42f95056f9ca4fa4958b5c9f25f5e4d91f60a12
Magus <magus@bgforge.net>
2026-03-03 21:52:03 +0700

    improved_equalizer: switch dialog to td

--- tnt/components/improved_equalizer/wsmith01.d
+++ tnt/components/improved_equalizer/wsmith01.d
@@ -1,29 +1,26 @@
 /** @tra improved_equalizer.tra */
+/* Generated from wsmith01.td - do not edit */
 
-// The transition list is shared between items with common price.
-// So we need to disable original transition and make new one.
-
-// Null original 
-ALTER_TRANS wsmith01    // file name
-BEGIN 32 END            // state number
-BEGIN 0 END             // transition number
-BEGIN                   // list of changes, see below for flags
+ALTER_TRANS wsmith01
+BEGIN 32 END
+BEGIN 0 END
+BEGIN
   "TRIGGER" ~False()~
 END
 
-// Add new state, copy transitions
 APPEND wsmith01
-    IF ~~ g_2things SAY @2
-    COPY_TRANS wsmith01 54
+    IF ~~ g_2things
+        SAY @2
+        COPY_TRANS wsmith01 54
     END
 END
 
-// Alter price in new transitions
-REPLACE_TRANS_TRIGGER wsmith01 BEGIN g_2things END BEGIN END ~PartyGoldGT(7499)~ ~PartyGoldGT(12499)~
-REPLACE_TRANS_ACTION wsmith01 BEGIN g_2things END BEGIN END ~TakePartyGold(7500)~ ~TakePartyGold(12500)~
-REPLACE_TRANS_ACTION wsmith01 BEGIN g_2things END BEGIN END ~DestroyGold(7500)~ ~DestroyGold(12500)~
+REPLACE_TRANS_TRIGGER wsmith01 BEGIN g_2things END BEGIN  END ~PartyGoldGT(7499)~ ~PartyGoldGT(12499)~
 
-// Add new transition to the root state
+REPLACE_TRANS_ACTION wsmith01 BEGIN g_2things END BEGIN  END ~TakePartyGold(7500)~ ~TakePartyGold(12500)~
+
+REPLACE_TRANS_ACTION wsmith01 BEGIN g_2things END BEGIN  END ~DestroyGold(7500)~ ~DestroyGold(12500)~
+
 EXTEND_TOP wsmith01 32
-  +~~+ @3 DO ~SetGlobal("ForgeItem","GLOBAL",5)~ + g_2things
+    ++ @3 DO ~SetGlobal("ForgeItem","GLOBAL",5)~ + g_2things
 END
